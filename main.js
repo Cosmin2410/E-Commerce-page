813ff9d7a9484481c4d116c484f264cd6c1a68bc
Nae Cosmin <cosminnae00@yahoo.com>
2022-03-09 22:31:34 +0200

    image-carousel for desktop added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Navigation
+
 const hamburgerMenu = document
   .querySelector('.ham-menu')
   .addEventListener('click', showPhoneMenu);
@@ -17,3 +19,61 @@ function closePhoneMenu() {
   phoneMenu.classList.remove('active');
   background.style.display = 'none';
 }
+
+// Images
+
+let firstClick = document.querySelector('.toggle');
+let secondClick;
+let counter = 1;
+
+const allMainImages = document.querySelectorAll('.main-image');
+const firstMainImage = document.querySelector('.one');
+const secondMainImage = document.querySelector('.two');
+
+const thumbnails = document.querySelectorAll('.thumbnail');
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', () => {
+    counter++;
+    if (counter % 2 === 0) {
+      firstClick.classList.remove('toggle', 'change');
+      secondClick = thumbnail;
+      secondClick.classList.add('change');
+    } else {
+      secondClick.classList.remove('change');
+      firstClick = thumbnail;
+      firstClick.classList.add('change');
+    }
+
+    allMainImages.forEach((allMainImage) => {
+      if (
+        thumbnail.classList.contains('one') &&
+        allMainImage.classList.contains('one')
+      ) {
+        allMainImage.classList.add('show');
+      } else {
+        allMainImage.classList.remove('show');
+      }
+
+      if (
+        thumbnail.classList.contains('two') &&
+        allMainImage.classList.contains('two')
+      ) {
+        allMainImage.classList.add('show');
+      }
+
+      if (
+        thumbnail.classList.contains('three') &&
+        allMainImage.classList.contains('three')
+      ) {
+        allMainImage.classList.add('show');
+      }
+
+      if (
+        thumbnail.classList.contains('four') &&
+        allMainImage.classList.contains('four')
+      ) {
+        allMainImage.classList.add('show');
+      }
+    });
+  });
+});
